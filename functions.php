8d3626a49f3fd64a8ebae570ae81efdcfc96b028
Nicolò Paganelli <n.paganelli@theorangeseed.it>
2022-05-11 15:54:36 +0200

    Sistemata indentazione

--- functions.php
+++ functions.php
@@ -5,19 +5,19 @@
  */
 function perf_enqueue_styles()
 {
-	wp_enqueue_style(
-		'child-style',
-		get_stylesheet_uri(),
-		array( 'twenty-twenty-one-style' ),
-		wp_get_theme()->get('Version')
-	);
+    wp_enqueue_style(
+        'child-style',
+        get_stylesheet_uri(),
+        array( 'twenty-twenty-one-style' ),
+        wp_get_theme()->get('Version')
+    );
 
-	wp_enqueue_style(
-		'child-custom-style',
-		get_stylesheet_directory_uri() . '/custom.css',
-		array( 'twenty-twenty-one-style' ),
-		wp_get_theme()->get('Version')
-	);
+    wp_enqueue_style(
+        'child-custom-style',
+        get_stylesheet_directory_uri() . '/custom.css',
+        array( 'twenty-twenty-one-style' ),
+        wp_get_theme()->get('Version')
+    );
 }
 add_action( 'wp_enqueue_scripts', 'perf_enqueue_styles' );
 
@@ -26,29 +26,29 @@ add_action( 'wp_enqueue_scripts', 'perf_enqueue_styles' );
  */
 function perf_widgets_init()
 {
-	register_sidebar(
-		array(
-			'name'          => __( 'Sidebar di esempio', 'performize' ),
-			'id'            => 'example-sidebar',
-			'description'   => __( 'Prova sidebar.', 'performize' ),
-			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
-		)
-	);
+    register_sidebar(
+        array(
+            'name'          => __( 'Sidebar di esempio', 'performize' ),
+            'id'            => 'example-sidebar',
+            'description'   => __( 'Prova sidebar.', 'performize' ),
+            'before_widget' => '<section id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</section>',
+            'before_title'  => '<h2 class="widget-title">',
+            'after_title'   => '</h2>',
+            )
+    );
     
-	register_sidebar(
-		array(
-			'name'          => __( 'Custom Sidebar', 'performize' ),
-			'id'            => 'custom-sidebar',
-			'description'   => __( 'Sidebar per custom template.', 'performize' ),
-			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
-		)
-	);
+    register_sidebar(
+        array(
+            'name'          => __( 'Custom Sidebar', 'performize' ),
+            'id'            => 'custom-sidebar',
+            'description'   => __( 'Sidebar per custom template.', 'performize' ),
+            'before_widget' => '<section id="%1$s" class="widget %2$s">',
+            'after_widget'  => '</section>',
+            'before_title'  => '<h2 class="widget-title">',
+            'after_title'   => '</h2>',
+        )
+    );
 }
 add_action( 'widgets_init', 'perf_widgets_init' );
 
@@ -58,10 +58,10 @@ add_action( 'widgets_init', 'perf_widgets_init' );
  */
 function perf_register_menu() //da back-office se non seleziono niente in 'Posizione di visualizzazione', viene mostrato comunque
 {
-	register_nav_menus(
-		array(
-			'custom-menu'  => __( 'Custom menu', 'performize' ),
-		)
-	);
+    register_nav_menus(
+        array(
+            'custom-menu'  => __( 'Custom menu', 'performize' ),
+        )
+    );
 }
 add_action( 'after_setup_theme', 'perf_register_menu' );
